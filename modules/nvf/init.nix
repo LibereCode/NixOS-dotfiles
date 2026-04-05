16321385c8f7c9df5652af235dcc411bbd023ef2
LibereCode <libere-code@posteo.org>
2026-04-05 23:23:47 +0200

    neovim NVF

--- modules/nvf/init.nix
+++ modules/nvf/init.nix
@@ -8,6 +8,17 @@ programs.nvf = {
                 lsp = {
                         enable = true;
                 };
-        };
+                options = {
+                cmdheight = 2;
+                cursorlineopt = "both";
+                mouse = "nvc";
+                splitright = true;
+                splitbelow = true;
+                shiftwidth = 4;
+                tabstop = 4;
+                softtabstop = 4;
+                wrap = false;
+                };
+    };
 };
 }
